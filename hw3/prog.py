6d3b5c362342682afb5be330975c8f71d1f918e3
Aleksey <amstarchenko@edu.hse.ru>
2018-12-02 01:03:14 +0300

    add hw3

---
+++ hw3/prog.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask import render_template, request, redirect, jsonify
+from collections import Counter
+
+app = Flask(__name__)
+
+NAMES = [
+    'i', 'you', 'he', 'we', 'youpl', 'they', 'this', 'that', 'here', 'there',
+    'lang', 'sex'
+]
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/write')
+def write():
+    args_list = []
+
+    for a in NAMES:
+        args_list.append(request.args.get(a, ''))
+
+    with open('data.csv',  'a+', encoding='utf-8') as f:
+        f.write('\t'.join(args_list) + '\r')
+
+    return redirect('/')
+
+@app.route('/json')
+def json_page():
+    with open('data.csv', encoding='utf-8') as f:
+        table = f.read().strip('\n')
+    res = [
+        {n: v for n, v in zip(NAMES, line.split('\t'))}
+        for line in table.split('\n')
+    ]
+
+    return jsonify(res)
+
+@app.route('/search')
+def search():
+    with open('data.csv', encoding='utf-8') as f:
+        table = f.read().strip('\n')
+    res = [line.split('\t') for line in table.split('\n')]
+
+    word = request.args.get('word', '')
+    sex = request.args.getlist('sex')
+    sex_i = NAMES.index('sex')
+    res = [r for r in res if r[sex_i] in sex and any(word in w for w in r[:10])]
+    return render_template('search.html', results=res)
+
+
+@app.route('/stats')
+def stats():
+    with open('data.csv', encoding='utf-8') as f:
+        table = f.read().strip('\n')
+    res = [line.split('\t') for line in table.split('\n')]
+    words = [word for line in res for word in line[:10] if word]
+
+    ctr = Counter(words)
+    return render_template(
+        'stats.html',
+         ctr=sorted(ctr.most_common(20), key=lambda x: (-x[1], x[0])),
+         total=len(words))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
