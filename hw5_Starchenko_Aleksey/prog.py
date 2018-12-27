5fb2a5ef16c70757a9685d498c56cd3a2230e5d4
Aleksey <amstarchenko@edu.hse.ru>
2018-12-28 00:50:07 +0300

    add texts

--- hw5_Starchenko_Aleksey/prog.py
+++ hw5_Starchenko_Aleksey/prog.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template, request, redirect, jsonify
 from collections import Counter
 
+from parse_texts import parse_texts
+
 
 app = Flask(__name__)
 
@@ -12,4 +14,6 @@ def form():
 
 
 if __name__ == '__main__':
+    for text in parse_texts('texts'):
+        print(text)
     app.run(debug=True)
